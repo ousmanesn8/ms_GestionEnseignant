486a456f8c47883f5fa66bc5b1208bebeaea4476
uvsq21706624 <ouzmane84@gmail.com>
2019-11-25 14:27:34 +0100

    initialisation projet microservice enseignant

---
+++ src/main/java/microservice/enseignant/ms_GestionEnseignant/MsGestionEnseignantApplication.java
@@ -0,0 +1,13 @@
+package microservice.enseignant.ms_GestionEnseignant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionEnseignantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGestionEnseignantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/microservice/enseignant/ms_GestionEnseignant/MsGestionEnseignantApplicationTests.java
@@ -0,0 +1,13 @@
+package microservice.enseignant.ms_GestionEnseignant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionEnseignantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
